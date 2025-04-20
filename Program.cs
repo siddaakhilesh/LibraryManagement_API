7ac4ddb02cdfb6a91fc62e4fcbeb52cc6f9c52f3
Sidda Akhilesh <sidda.akhilesh@vialto.com>
2025-04-20 12:28:21 +0530

    configured logging and to generate new log files for each logging

--- Program.cs
+++ Program.cs
@@ -2,28 +2,31 @@ using LibraryAPI.Data;
 using LibraryAPI.Services;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure Serilog
+// Generate a unique filename based on current timestamp
+var logFilePath = $"Logs/log-{DateTime.Now:yyyyMMdd-HHmmss}.txt";
+
+// Configure Serilog to write to a new file on every run
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
-    .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .WriteTo.File(logFilePath)
     .CreateLogger();
 
 builder.Host.UseSerilog();
 
+// Add scoped logger service
 builder.Services.AddScoped<IDbLogger, DbLogger>();
-
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//Injecting database connection
+// Inject database connection
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryDbConnection")));
-
 
 var app = builder.Build();
 
